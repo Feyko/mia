ecfac14b9cd487289cf7073cd1ffe056075dc9e2
Feyko <feykook@gmail.com>
2022-12-05 22:14:50 +0100

    chore: remove useless comments

--- cmd/root.go
+++ cmd/root.go
@@ -1,18 +1,3 @@
-/*
-Copyright © 2021 Feyko
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 package cmd
 
 import (
@@ -29,20 +14,15 @@ import (
 
 var cfgFile string
 
-// rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "mia",
 	Short: "Easily download and watch releases of a multitude of media content",
 	Long: `Mia is a tool that lets you easily download and watch releases of a multitude of media content
 Once configured, simply use 'mia'
 You can also use 'mia watch' to ask mia to mia periodically`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
 	Run: func(cmd *cobra.Command, args []string) { mia.Run() },
 }
 
-// Execute adds all child commands to the root command and sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -53,31 +33,19 @@ func Execute() {
 func init() {
 	cobra.OnInitialize(initConfig)
 
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.mia.yaml)")
-
-	// Cobra also supports local flags, which will only mia
-	// when this action is called directly.
-	//rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" {
-		// Use config file from the flag.
 		viper.SetConfigFile(cfgFile)
 	} else {
-		// Find home directory.
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
-		// Search config in home directory with name ".mia" (without extension).
 		viper.SetConfigName(".mia")
 		viper.SetConfigType("yaml")
 		viper.AddConfigPath(home)
@@ -85,15 +53,14 @@ func initConfig() {
 
 	viper.SetDefault("enabledSources", sources.SupportedSources)
 
-	viper.AutomaticEnv() // read in environment variables that match
-	// If a config file is found, read it in.
+	viper.AutomaticEnv()
 	err := viper.ReadInConfig()
 	if err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	} else {
-		err := viper.SafeWriteConfig()
-		if err != nil {
-			log.Fatalf("Couldn't write config file: %v", err)
-		}
+		return
+	}
+
+	err = viper.SafeWriteConfig()
+	if err != nil {
+		log.Fatalf("Couldn't write config file: %v", err)
 	}
 }
--- go.mod
+++ go.mod
@@ -4,8 +4,6 @@ go 1.16
 
 require (
 	github.com/mitchellh/go-homedir v1.1.0
-	github.com/robfig/cron v1.2.0
 	github.com/spf13/cobra v1.2.1
 	github.com/spf13/viper v1.8.1
-	golang.org/x/tools v0.1.5 // indirect
 )
